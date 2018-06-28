3d99824f35602cb5d2207344936b5072827f523f
Kevin Capotosto <foexzya@yahoo.com>
2018-06-28 13:24:32 -0700

    added created/updated fields to members

--- Models/Members.cs
+++ Models/Members.cs
@@ -21,5 +21,11 @@ namespace goalswithfriends.Models
         public Groups group { get; set; }
 
 
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime created_at { get; set; }
+
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime updated_at { get; set; }
     }
 }
